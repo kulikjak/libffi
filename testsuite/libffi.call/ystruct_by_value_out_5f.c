825c28825797f18024ce3c5d17d6b8a749cfad84
Jakub Kulik <kulikjak@gmail.com>
2024-02-06 14:22:02 +0100

    more tests; now without arrays

---
+++ testsuite/libffi.call/ystruct_by_value_out_5f.c
@@ -0,0 +1,12 @@
+/* Area:	ffi_call
+   Purpose:	Check structures passed by value.
+   Limitations:	none.
+   PR:		TBD
+   Originator:	TBD  */
+
+/* { dg-do run } */
+#define NUM_ELEMS 5
+#define ELEM_TYPE float
+#define FFI_ELEM_TYPE ffi_type_float
+
+#include "ystruct_by_value_out.inc"
