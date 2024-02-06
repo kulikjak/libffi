be0827897505d6ef41e595ae1a174c11f26bcd71
Jakub Kulik <kulikjak@gmail.com>
2024-02-06 09:29:33 +0100

    better float & double struct by value tests
    Some of these do fail on SPARC

---
+++ testsuite/libffi.call/xstruct_by_value_in_5.c
@@ -0,0 +1,12 @@
+/* Area:	ffi_call
+   Purpose:	Check structures passed by value.
+   Limitations:	none.
+   PR:		TBD
+   Originator:	TBD  */
+
+/* { dg-do run } */
+#define NUM_ELEMS 5
+#define ELEM_TYPE double
+#define FFI_ELEM_TYPE ffi_type_double
+
+#include "xstruct_by_value_in.inc"
---
+++ testsuite/libffi.call/xstruct_by_value_out_4f.c
@@ -0,0 +1,12 @@
+/* Area:	ffi_call
+   Purpose:	Check structures passed by value.
+   Limitations:	none.
+   PR:		TBD
+   Originator:	TBD  */
+
+/* { dg-do run } */
+#define NUM_ELEMS 4
+#define ELEM_TYPE float
+#define FFI_ELEM_TYPE ffi_type_float
+
+#include "xstruct_by_value_out.inc"
